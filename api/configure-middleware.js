7ec33a3687552b3f72c4b093f0a7e0973d94e894
dueka <ekanemd@live.com>
2020-01-28 07:41:10 +0100

    created the sessions and cookies

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 module.exports = server => {
   server.use(helmet());
   server.use(express.json());
   server.use(cookieParser()); // 'cookie-parser'
   server.use(cors());
+  server.use(
+    session({
+      name: "notsession",
+      secret: "nobody tosses a dwarf!",
+      cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: true
+      },
+      httpOnly: true,
+      resave: true,
+      saveUninitialized: false
+    })
+  );
 };
